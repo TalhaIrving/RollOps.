e190fb4af982e8896b028cee0adbb428bb4c7819
TalhaIrving <talhairving@gmail.com>
2026-02-20 20:40:24 +0000

    Added session as separate route in app folder

--- app/main.py
+++ app/main.py
@@ -1,17 +1,10 @@
 from fastapi import FastAPI
-import uuid
+from app.routes.sessions import router as sessions_router
 
 app = FastAPI()
-
-sessions = {}
 
 @app.get("/health")
 def health_check():
     return {"status": "ok"}
 
-# create a session
-@app.post("/sessions")
-def create_session():
-    session_id = str(uuid.uuid4())
-    sessions[session_id] = {}
-    return {"session_id": session_id}
+app.include_router(sessions_router)
